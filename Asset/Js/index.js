b05b2fc872d20434c7d38ed6b646dffdfb036d0f
CamilaDanielaBanaresCarrasco <camilabanares.informaticos@gmail.com>
2023-04-22 11:13:27 -0400

    se agrega grafico a un metodo

--- Asset/Js/index.js
+++ Asset/Js/index.js
@@ -11,38 +11,7 @@ respuestaBoton.addEventListener(`click`,function(){
         dataType: "json",
         success: function (response) {
             console.log(response); //Verifico
-            //---------------------Trabajando con canvas---------------------------------------
-
-
- // window.onload = function () { Cada vez que se cargue la pagina hace esta funcion
-
-    var chart = new CanvasJS.Chart("chartContainer", { //instanciar un objeto de tipo CanvasJS
-        theme: "dark1", // "light2", "dark1", "dark2"
-        animationEnabled: true, // change to true		
-        title:{
-            text: response.name
-        },
-        data: [
-        {
-            type: "column",
-            dataPoints: [
-                { label: "Hp",  y: response.stats[0].base_stat  },
-                { label: "Attack", y: response.stats[1].base_stat  },
-                { label: "Defense", y: response.stats[2].base_stat  },
-                { label: "Ataque ataque", y: response.stats[3].base_stat  },
-                { label: "Ataque especial",  y: response.stats[4].base_stat  },
-                { label: "Defenza",  y: response.stats[5].base_stat  }
-            ]
-        }
-        ]
-    });
-    chart.render();
-    
-     // }
-
-
-//--------------------------------------------------------
-
+           crearGrafico(response);
 
         }
     });
@@ -51,3 +20,43 @@ respuestaBoton.addEventListener(`click`,function(){
 
 //--------------------
 
+function crearGrafico(response){
+
+ //---------------------Trabajando con canvas---------------------------------------
+
+
+ // window.onload = function () { Cada vez que se cargue la pagina hace esta funcion
+
+ var chart = new CanvasJS.Chart("chartContainer", { //instanciar un objeto de tipo CanvasJS
+    theme: "dark1", // "light2", "dark1", "dark2"
+    animationEnabled: true, // change to true		
+    title:{
+        text: response.name
+    },
+    data: [
+    {
+        type: "column",
+        dataPoints: [
+            { label: "Hp",  y: response.stats[0].base_stat  },
+            { label: "Attack", y: response.stats[1].base_stat  },
+            { label: "Defense", y: response.stats[2].base_stat  },
+            { label: "Ataque ataque", y: response.stats[3].base_stat  },
+            { label: "Ataque especial",  y: response.stats[4].base_stat  },
+            { label: "Defenza",  y: response.stats[5].base_stat  }
+        ]
+    }
+    ]
+});
+chart.render();
+
+ // }
+
+
+//--------------------------------------------------------
+
+
+
+
+
+
+}
