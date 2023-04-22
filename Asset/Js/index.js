6436367a82b58d57a1e0dca12921393b1e661eca
CamilaDanielaBanaresCarrasco <camilabanares.informaticos@gmail.com>
2023-04-22 10:40:17 -0400

    se corrige grafico

--- Asset/Js/index.js
+++ Asset/Js/index.js
@@ -1,6 +1,6 @@
    //intrucciones 
 
-
+//-----------------------Consumiendo API FORMA DEL PROFE----------------------------
 var respuestaBoton = document.querySelector("#btnConsulta")
 respuestaBoton.addEventListener(`click`,function(){
     var txtNombrePokemon = document.querySelector("#nombrePokemon").value; // el valor de lo que contiene la etiqueta con el id nombrePokemon
@@ -10,12 +10,44 @@ respuestaBoton.addEventListener(`click`,function(){
         url: "https://pokeapi.co/api/v2/pokemon/"+ txtNombrePokemon,
         dataType: "json",
         success: function (response) {
-            console.log(response);
+            console.log(response); //Verifico
+            //---------------------Trabajando con canvas---------------------------------------
+
+
+ // window.onload = function () { Cada vez que se cargue la pagina hace esta funcion
+
+    var chart = new CanvasJS.Chart("chartContainer", { //instanciar un objeto de tipo CanvasJS
+        theme: "dark1", // "light2", "dark1", "dark2"
+        animationEnabled: true, // change to true		
+        title:{
+            text: "pokemon"
+        },
+        data: [
+        {
+            type: "column",
+            dataPoints: [
+                { label: "Hp",  y: response.stats[0].base_stat  },
+                { label: "Attack", y: response.stats[1].base_stat  },
+                { label: "Defense", y: response.stats[2].base_stat  },
+                { label: "Ataque ataque", y: response.stats[3].base_stat  },
+                { label: "Ataque especial",  y: response.stats[4].base_stat  },
+                { label: "Defenza",  y: response.stats[5].base_stat  }
+            ]
+        }
+        ]
+    });
+    chart.render();
+    
+     // }
+
+
+//--------------------------------------------------------
+
+
         }
     });
 });
 
 
-
-
+//--------------------
 
